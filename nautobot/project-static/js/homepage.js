cd12fef514a3ae33d53faff0b22f915223e25a2e
Matt Vitale <matt.vitale@networktocode.com>
2023-04-14 12:08:59 -0400

    Testing button toggle

--- nautobot/project-static/js/homepage.js
+++ nautobot/project-static/js/homepage.js
@@ -3,10 +3,10 @@ window.onload = function () {
     var cookies = document.cookie.split(';');
     // Loop through each cookie found
     for(var i = 1; i <= cookies.length; i++) {
-        // We only care about cookies with '_homepage_collapsed' in the name
-        if (cookies[i - 1].includes("_homepage_collapsed")) {
-            // Remove '_hompage_collapsed' string from Cookie name
-            var cookie_name = cookies[i - 1].replace("_homepage_collapsed", "")
+        // We only care about cookies with '_homepage_collapse' in the name
+        if (cookies[i - 1].includes("_homepage_collapse")) {
+            // Remove '_hompage_collapse' string from Cookie name
+            var cookie_name = cookies[i - 1].replace("_homepage_collapse", "")
             // Split on '=' sign and get the name of the Div only. Trim whitespace
             var name_value = cookie_name.split("=")[0].trim()
             // Split on '=' sign and get value (True/False) only. Trim whitespace
@@ -19,20 +19,25 @@ window.onload = function () {
             // If the cookie is set to hide the panel, do so here
             if (cookie_value == "True") {
                 // Add 'collapsed' class attribute to Button
-                button.classList.add("collapsed");
+                // button.classList.add("collapsed");
                 // Remove 'in' class attribute from Panel (this defaults the panel to display)
                 panel.classList.remove("in");
+                hidePanel(button);
+                console.log(cookies[i - 1]);
             }
             else {
                 // Remove 'collapsed' class attribute to Button
-                button.classList.remove("collapsed");
+                // button.classList.remove("collapsed");
                 // Add 'in' class attribute from Panel (this defaults the panel to display)
                 panel.classList.add("in");
+                showPanel(button);
             }
         }
     }
 };
 
+// TODO: Fix button not initially toggling the FIRST time I click it, but works every other time
+ 
 // Toggle homepage Div panel on button click
 function toggleHomepagePanel(item) {
     // Get the dynamic page button element using the ID of the button that was clicked
@@ -42,15 +47,32 @@ function toggleHomepagePanel(item) {
     var panel_name = button.id.replace("_btn", "");
     // var panel = document.getElementById(panel_name + "_content");
 
-    
     // This is actually the state when the button is clicked, so we take the inverse of it
     // Eg: If collapsed is True, that means it WAS collapsed, and the user clicked it to expand it
     if ($(button).hasClass("collapsed")) {
         // Set the cookie to False
-        document.cookie = `${panel_name}_homepage_collapsed=False; path=/`;
+        console.log("True");
+        document.cookie = `${panel_name}_homepage_collapse=False; path=/`;
+        hidePanel(button);
     }
     else {
+        console.log("False");
         // Set the cookie to True
-        document.cookie = `${panel_name}_homepage_collapsed=True; path=/`;
+        document.cookie = `${panel_name}_homepage_collapse=True; path=/`;
+        showPanel(button);
     }
+}
+
+function hidePanel(button) {
+    button.classList.remove("btn-danger");
+    button.classList.remove("glyphicon-minus");
+    button.classList.add("btn-success");
+    button.classList.add("glyphicon-plus");
+}
+
+function showPanel(button) {
+    button.classList.remove("btn-success");
+    button.classList.remove("glyphicon-plus");
+    button.classList.add("btn-danger");
+    button.classList.add("glyphicon-minus");
 }
